6400564ffb734ad6778d180f06cc2995951e37f1
AnJaLi21Patil <angalipatilsonu2003@gmail.com>
2026-02-22 16:25:28 +0530

    Configured PostgreSQL database and connected FastAPI

--- app/database.py
+++ app/database.py
@@ -2,16 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./fitness.db"
+DATABASE_URL = "postgresql://postgres:pass%40123@localhost:5433/fitness_db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
